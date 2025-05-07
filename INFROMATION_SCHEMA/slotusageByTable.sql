7a987efa5b5a1eb4580dd065506c2660bbc8025a
prakashmahali <prakash.mahali@gmail.com>
2025-05-07 16:36:43 -0500

    Update slotusageByTable.sql

--- INFROMATION_SCHEMA/slotusageByTable.sql
+++ INFROMATION_SCHEMA/slotusageByTable.sql
@@ -18,3 +18,22 @@ WHERE
     FROM UNNEST(referenced_tables) AS t
     WHERE t.table_id = 'order_data'
   )
+#Aggregated by hour 
+SELECT
+  TIMESTAMP_TRUNC(start_time, HOUR) AS hour,
+  statement_type,
+  COUNT(*) AS job_count,
+  SUM(total_slot_ms) AS total_slot_ms,
+  ROUND(SUM(total_slot_ms) / 1000.0, 2) AS slot_seconds
+FROM
+  `region-us`.INFORMATION_SCHEMA.JOBS_BY_PROJECT
+WHERE
+  DATE(start_time) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+  AND statement_type IN ('QUERY', 'INSERT', 'MERGE')
+  AND EXISTS (
+    SELECT 1
+    FROM UNNEST(referenced_tables) AS t
+    WHERE t.table_id = 'order_data'
+  )
+GROUP BY hour, statement_type
+ORDER BY hour;
